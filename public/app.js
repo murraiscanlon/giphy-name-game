4a5106f16ca7b3d18d3ee73f767197fef2d0f2ac
murrai <murrai.scanlon@gmail.com>
2021-04-18 17:54:53 -0500

    test add second gif

--- public/app.js
+++ public/app.js
@@ -1,18 +1,23 @@
 //create variable that stores button in a JavaScript variable
 let firstButton = document.querySelector("#firstButton")
+let secondButton = document.querySelector("#secondButton")
 let gifs;
 
 //add an event listener to the button.
 //Run the function sendApiRequest when the button is clicked
 firstButton.addEventListener("click", () => {
-    sendApiRequest() //API, "#input
+    sendApiRequest('#firstInput') //API, "#input
+})
+
+secondButton.addEventListener("click", () => {
+    sendApiRequest('#secondInput') //API, "#input
 })
 
 //fetch data from the API
-async function sendApiRequest() { //API_KEY
+async function sendApiRequest(buttonId) { //API_KEY
 
     //get the user input value from the input text field
-    let userInput = document.querySelector('#firstInput').value;
+    let userInput = document.querySelector(buttonId).value;
 
     //send the request for data. Hope for status code: 200
     let response = await fetch(`/giphySearch/${userInput}`);
@@ -30,6 +35,7 @@ async function sendApiRequest() { //API_KEY
 //do something with the API data you've just received
 function useApiData(gifs) {
     document.querySelector('#firstImage').innerHTML = `<img id="gif" src="${gifs.data[0].images.original.url}">`
+    document.querySelector('#secondImage').innerHTML = `<img id="gif" src="${gifs.data[0].images.original.url}">`
 }
 
 
